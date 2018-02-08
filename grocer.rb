8a5534e087ad6462389ffdff818e79e12edff457
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-08 19:02:11 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -20,14 +20,12 @@ def apply_coupons(cart, coupons)
     newCount = 0
     newItemCount = 0
     newItemCost = 0.00
-    newItemName = ""
     coupons.each {|coupon|
       if coupon[:item] == item
         if coupon[:num] <= item[:count]
           newCount = item[:count] % coupon[:num]
           newItemCount = item[:count] / coupon[:num]
           newItemCost = coupon[:cost]
-          newItemName = item + "W/COUPON"
         end
       end
     }
@@ -35,7 +33,7 @@ def apply_coupons(cart, coupons)
       if newCount
         appliedCart[item] = {:price => item[:price], :clearance => item[:clearance], :count => newCount}
       end
-      appliedCart[item + "W/COUPON"] = {:price => newItemCost, :clearance => item[:clearance], :count => newItemCount}
+      appliedCart[item + " W/COUPON"] = {:price => newItemCost, :clearance => item[:clearance], :count => newItemCount}
     else
       appliedCart[item] = values
     end
