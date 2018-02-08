5c33a657d713ce81133fb23ff721ed8dbd552baa
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-08 19:29:16 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -40,7 +40,9 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  cart = apply_coupons(consolidate_cart(cart), coupons)
+  cart = consolidate_cart(cart)
+  binding.pry
+  cart = apply_coupons(cart, coupons)
   cart = apply_clearance(cart)
   total = 0.00
   cart.each {|item|
